2ee691526d7615d9798d5c0e032ce761f3926afc
Asim401 <asimmousa@ymail.com>
2021-02-07 16:49:22 -0800

    ss

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Welcome To Al-Hussen Youth Club")
+var fname = "Asim Ahmad";
+console.log (fname);
+console.log ("First Name is  " + fname);
+var age = 23;
+console.log (age);
+var isFan = "true "
+console.log (isFan);
+console.log (fname + ' ,' + age + "Years old , is a Fan" + ' ' + isFan );
+var fname  = prompt("What is your name") ;
+if(fname = 'Asim Ahmad') {
+console.log('Welcome Asim')
+} else {
+   console.log ('sign with us and become a fan');
+}
+var age  = prompt("How Old are You");
+
